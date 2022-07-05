b74ed0caa103eda3eac1b0f66478b1603c23d91a
bonettimrc <bonetti.mrc@gmail.com>
2022-07-05 12:04:59 +0200

    fixed wget install again

--- index.sh
+++ index.sh
@@ -16,10 +16,10 @@ gitInstall(){
   sudo make install --directory=$directory
 }
 wgetInstall(){
-  file="binaries-directory/$1.deb"
+  file="./binaries-directory/$1.deb"
   url=$(jq --raw-output ".[] | select(.name==\"$1\") | .url" programs.json)
   wget $url --output-document="$file"
-  sudo apt install $file
+  sudo apt install "$file"
 }
 complicatedInstall(){
   case $1 in
